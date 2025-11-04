0a8ddd5c4b7990bbd1a8fa2f02084fe8896dcc00
Yuly Caraballo <vanehill@gmail.com>
2025-11-04 01:02:20 +0100

    docs: mejorar documentación de configuración

--- run.php
+++ run.php
@@ -1,5 +1,16 @@
 <?php
-
+/**
+ * Script principal de ejecución del sitio web
+ * 
+ * Este archivo actúa como punto de entrada principal de la aplicación.
+ * Configura el entorno, carga las dependencias necesarias e inicializa
+ * el sistema de plantillas.
+ *
+ * @package  DEAW_Project
+ * @author   [Yuly Caraballo] <vanehill@gmail.com>
+ * @version  v3.1
+ * @since    2025-11-03
+ */
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -7,5 +18,10 @@ ini_set('display_errors', 1);
 require __DIR__ . '/includes/config.php';
 require __DIR__ . '/includes/functions.php';
 
-
+/**
+ * Inicia la aplicación cargando la plantilla principal
+ * 
+ * @return void
+ */
 init();
+
